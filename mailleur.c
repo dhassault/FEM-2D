00d6d0f024af06e0db7a1c8243a78159f87ff274
dhassault <yann.leguilly@gmail.com>
2016-02-07 14:46:02 +0100

    Update mailleur.c

--- mailleur.c
+++ mailleur.c
@@ -4,21 +4,54 @@
 
 int main() {
   char a;
+  char filename[20]="mesh.dat";
+  int i,m,n,p,q,t;
+  int j; //pour tester sur printf avec lecfima, peut être supprimé après
+  int **nRefAr,**ngnel;
+  float **coord;
   printf("Do you want to create a custom mesh file ? [y]es or [n]o\n");
   scanf("%c",&a);
-  if (a=="y") {
-    void createMeshFile();
+  if (a=='y') {
+  createMeshFile();
   }
+
   /****************************
   Reading of a mesh file
   *****************************/
-  float **coord;
-  //char *ficmai = filename;
-  int nbtng;
-  float ***pcoord=&coord;
+  char *ficmai=filename;
+  int *nbtel=&m;
+  int *nbneel=&p;
+  int *nbtng=&n;
+  int *nbaret=&q;
+  int *nutyge=&t;
   int ***pnRefAr=&nRefAr;
+  int ***pngnel=&ngnel;
+  float ***pcoord=&coord;
 
-  //pnRefAr=lecfima(ficmai,nbtng,pcoord,nbtel,pngnel,nbneel,nbaret,pnRefAr); //remember to add nutyge on university's computers
-
+  lecfima(ficmai,nutyge,nbtng,pcoord,nbtel,pngnel,nbneel,nbaret,pnRefAr); //remember to add nutyge on university's computers
+  printf("There is %d geometrical nodes.\n",n);
+  printf("m=%d t=%d p=%d q=%d\n",m,t,p,q);
+  ///************************************************
+  printf("The coordiantes are:\n");
+  for (i=0;i<n;i++){
+    printf("%f %f\n",coord[i][0],coord[i][1]);
+  }
+  printf("ngnel:-----------\n");
+  for (i=0;i<m;i++){
+    for (j=0;j<p;j++){
+      printf("%d ",ngnel[i][j]);
+    }
+    printf("\n");
+  }
+  printf("-----------\n");
+  printf("nRefAr:------------\n");
+  for (i=0;i<m;i++){
+    for (j=0;j<q;j++){
+      printf("%d ",nRefAr[i][j]);
+    }
+    printf("\n");
+  }
+  printf("-----------\n");
+  //*******************************************/
   return 0;
 }
