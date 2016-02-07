9b803a0fb06efc8f7a3a9d5eaa02f331a9cc65dc
dhassault <yann.leguilly@gmail.com>
2016-02-07 14:46:46 +0100

    Update fonctions.h

--- fonctions.h
+++ fonctions.h
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <math.h>
 
-void etiqar(int typel, int n1, int n2, int nrefdom, const int *nrefcot, int nbtel, int nbaret, int **nRefAr);
 int **alloctabINT(int dim1, int dim2);
 float **alloctabFLOAT(int dim1, int dim2);
-int lecfima(char *ficmai, int *nutyge, int *nbtng, float ***pcoord, int *nbtel, int ***pngnel, int *nbneel, int *nbaret, int ***pnRefAr);
+void etiqar(int typel, int n1, int n2, int nrefdom, const int *nrefcot, int nbtel, int nbaret, int **nRefAr);
+void lecfima(char *ficmai, int *nutyge, int *nbtng, float ***pcoord, int *nbtel, int ***pngnel, int *nbneel, int *nbaret, int ***pnRefAr);
 void createMeshFile();
